38d507b147cd483aaea0b7508413b7648e1c5c52
Andre Cordeiro <andrezaocordeiro@gmail.com>
2024-01-08 11:46:32 -0300

    feat: create and get orders

--- backend/cmd/order/order.go
+++ backend/cmd/order/order.go
@@ -1,8 +1,10 @@
 package order
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/AndreCordeir0/fullstack-challenge/backend/cmd/pizza"
 	"github.com/AndreCordeir0/fullstack-challenge/backend/database"
@@ -10,9 +12,17 @@ import (
 )
 
 type Order struct {
-	Pizza    pizza.Pizza
-	IdPizza  int `json:"id_pizza"`
-	Quantity int `json:"quantity"`
+	Id         int         `json:"id"`
+	Name       string      `json:"name"`
+	Adress     string      `json:"adress"`
+	OrderItems []OrderItem `json:"order_items"`
+}
+type OrderItem struct {
+	Id       int
+	IdPizza  int         `json:"id_pizza"`
+	Quantity int         `json:"quantity"`
+	IdOrder  int         `json:"-"`
+	Pizza    pizza.Pizza `json:"-"`
 }
 
 func Create(c *gin.Context) {
@@ -29,25 +39,111 @@ func Create(c *gin.Context) {
 	}
 	defer db.Close()
 	defer tx.Rollback()
-	result, txError := tx.Exec("INSERT INTO ORDER_PIZZA (id_pizza, quantity) VALUES (?, ?)", order.IdPizza, order.Quantity)
-	if txError != nil {
-		//TODO
-		c.JSON(500, "")
-		return
-	}
-	id, idError := result.LastInsertId()
-	if idError != nil {
-		//TODO
 
-		c.JSON(500, "")
+	orderId, err := order.CreateOrder(tx)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(500, gin.H{"error": err})
 		return
 	}
-	location := fmt.Sprintf("orders/%d", id)
+
+	orderItemsError := order.CreateOrderItems(tx, orderId)
+	if orderItemsError != nil {
+		fmt.Println(orderItemsError)
+		c.JSON(500, gin.H{"error": orderItemsError})
+		return
+	}
+
+	errCommit := tx.Commit()
+	if errCommit != nil {
+		fmt.Println("Commit error", errCommit)
+		c.JSON(500, errCommit)
+		return
+	}
+
+	location := fmt.Sprintf("orders/%d", orderId)
 	c.Writer.Header().Add("Location", location)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  http.StatusCreated,
+		"message": "created order",
+	})
 }
 
 func Get(c *gin.Context) {
+	db := database.GetConnection()
+	tx, err := db.Begin()
 
+	if err != nil {
+		log.Fatal("Error getting transaction", err)
+	}
+	defer db.Close()
+
+	result, queryErr := tx.Query("SELECT * FROM ORDER_PIZZA")
+	if queryErr != nil {
+		c.AbortWithStatusJSON(500, queryErr.Error())
+	}
+
+	var orders []Order
+	for result.Next() {
+		var order Order
+		if err := result.Scan(&order.Id, &order.Name, &order.Adress); err != nil {
+			c.JSON(500, err.Error())
+			return
+		}
+		orderItems, err := GetOrderItems(order.Id)
+		if err != nil {
+			c.JSON(500, err.Error())
+			return
+		}
+		order.OrderItems = orderItems
+		orders = append(orders, order)
+	}
+
+	c.JSON(200, orders)
+}
+
+func GetOrderItems(orderId int) ([]OrderItem, error) {
+	db := database.GetConnection()
+	tx, err := db.Begin()
+
+	if err != nil {
+		log.Fatal("Error getting transaction", err)
+	}
+	defer db.Close()
+
+	result, queryErr := tx.Query("SELECT * FROM ORDER_ITEM o WHERE o.id_order = $1", orderId)
+	if queryErr != nil {
+		return []OrderItem{}, queryErr
+	}
+	var orderItems []OrderItem
+	for result.Next() {
+		var orderItem OrderItem
+		if err := result.Scan(&orderItem.Id, &orderItem.IdPizza, &orderItem.Quantity, &orderItem.IdOrder); err != nil {
+			return []OrderItem{}, err
+		}
+		orderItems = append(orderItems, orderItem)
+	}
+	return orderItems, nil
+}
+
+func (o *Order) CreateOrder(tx *sql.Tx) (lastInsertedId int, txError error) {
+	txError = tx.QueryRow("INSERT INTO ORDER_PIZZA (name, adress) VALUES ($1, $2) RETURNING id", o.Name, o.Adress).Scan(&lastInsertedId)
+	if txError != nil {
+		return 0, txError
+	}
+	return lastInsertedId, nil
+}
+
+func (o *Order) CreateOrderItems(tx *sql.Tx, orderId int) error {
+	fmt.Println("Order id: ", orderId)
+	for _, item := range o.OrderItems {
+		_, txError := tx.Exec("INSERT INTO ORDER_ITEM (id_pizza, quantity, id_order) VALUES ($1, $2, $3)", item.IdPizza, item.Quantity, orderId)
+		if txError != nil {
+			return txError
+		}
+	}
+	return nil
 }
 
 func GetById(c *gin.Context) {
